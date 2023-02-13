b85d6943653347eb6e2243ceb353b6ddab1eeafc
Ruhan Xia <xia.ruh@northeastern.edu>
2023-02-13 15:20:02 -0500

    Update main.js
    update

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 // JS File for ic-06
+// dynamic styling
+function submit2Clicked() {
+	let line = document.getElementById('line');
+
+	line.classList.add('red');
+}
+
+document.getElementById('button1')
+		.addEventListener('click', submit2Clicked);
+
+
+item = document.getElementById('circle');
+item.addEventListener("mouseover", func, false);
+item.addEventListener("mouseout", func1, false);
+
+function func() {
+	let item = document.getElementById('circle');
+	item.addEventListener("mouseover", func, false);
+	item.addEventListener("mouseout", func1, false);
+
+	line.classList.add('orange');
+}
+
+function func1()
+{  
+   item.setAttribute("style", "background-color:green;")
+}
+
